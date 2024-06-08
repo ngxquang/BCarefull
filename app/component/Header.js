245e501108ab19b4cd9103dbabd7562b3f18d6c6
Ngyyen <ngyyenle47@gmail.com>
2024-06-08 10:03:37 +0700

    fix UI

--- app/component/Header.js
+++ app/component/Header.js
@@ -1,45 +1,45 @@
-import * as React from "react";
-import { Header, Icon } from "@rneui/base";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { BCarefulTheme } from "./Theme";
-import { StyleSheet, View } from "react-native";
-import Fonts from "../../assets/fonts/Fonts";
-import { BackToHomeBtn, GoBackBtn } from "./ButtonHome";
-import Progress from "./datLich/Progress";
+import * as React from 'react';
+import {Header, Icon} from '@rneui/base';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {BCarefulTheme} from './Theme';
+import {StyleSheet, View} from 'react-native';
+import Fonts from '../../assets/fonts/Fonts';
+import {BackToHomeBtn, GoBackBtn} from './ButtonHome';
+import Progress from './datLich/Progress';
 
-export function CustomHeader({ title, leftIcon = GoBackBtn, rightIcon = null }) {
-    return (
-        <View>
-            <Header
-                backgroundColor="white"
-                backgroundImageStyle={{}}
-                barStyle="default"
-                centerComponent={{
-                    text: title,
-                    style: styles.header
-                }}
-                centerContainerStyle={{ alignContent: 'flex-start' }}
-                leftComponent={leftIcon}
-                leftContainerStyle={{ left: 10}}
-                linearGradientProps={{}}
-                placement="center"
-                statusBarProps={{}}
-                rightComponent={rightIcon}
-                rightContainerStyle={{ right: 10 }}
-            />
-        </View>
-    );
+export function CustomHeader({title, leftIcon = GoBackBtn, rightIcon = null}) {
+  return (
+    <View>
+      <Header
+        backgroundColor="white"
+        backgroundImageStyle={{}}
+        barStyle="default"
+        centerComponent={{
+          text: title,
+          style: styles.header,
+        }}
+        centerContainerStyle={{alignContent: 'flex-start'}}
+        leftComponent={leftIcon}
+        leftContainerStyle={{left: 10}}
+        linearGradientProps={{}}
+        placement="center"
+        statusBarProps={{}}
+        rightComponent={rightIcon}
+        rightContainerStyle={{right: 10}}
+      />
+    </View>
+  );
 }
 
-export function DatLichHeader({ title, values }) {
-    return (
-        <>
-            <CustomHeader title={title} />
-            <View style={{padding: 30, paddingBottom: 0}}>
-                <Progress values={values ? values : 0} />
-            </View>
-        </>
-    )
+export function DatLichHeader({title, values}) {
+  return (
+    <>
+      <CustomHeader title={title} />
+      <View style={{padding: 30, backgroundColor: '#fff'}}>
+        <Progress values={values ? values : 0} />
+      </View>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
